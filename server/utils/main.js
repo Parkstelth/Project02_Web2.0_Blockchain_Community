58ce4d00f9596e065b34ebe69ce67cb8f5381015
Ubuntu <ubuntu@instance-4.asia-northeast3-b.c.robotic-weft-332901.internal>
2021-12-28 07:42:10 +0000

    m

--- server/utils/main.js
+++ server/utils/main.js
@@ -19,7 +19,7 @@ const contractAddress = fs.readFileSync(
 );
 
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 const allTransactions = [];
 let lastest = checkedBlockNum;
 
